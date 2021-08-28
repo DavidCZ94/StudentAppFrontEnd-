5cd28df24cadbee4dfa75fbb69aa9a4b3acb8061
DavidCZ94 <david940103@gmail.com>
2021-08-28 03:16:04 -0500

    Students tables finished

---
+++ src/app/core/models/student.model.ts
@@ -0,0 +1,14 @@
+export interface Student{
+    id: number,
+    name1: string,
+    name2: string,
+    lastName1: string,
+    lastName2: string,
+    city: string,
+    direction: string,
+    email: string,
+    identification: string,
+    identificationType: string,
+    phone: string,
+    studentCourses?: [],
+};
